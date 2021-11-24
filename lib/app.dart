5dbc51f44847ea22adc900c4a2fcfda784503ba0
SeungHyun Min <contact@seunghyun.in>
2021-11-24 22:11:57 +0900

    Create app.dart

---
+++ lib/app.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class SilgamApp extends StatelessWidget {
+  const SilgamApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Silgam',
+    );
+  }
+}
