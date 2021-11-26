6da299775b5e83250dba2f8fbd44666ed6285c3b
SeungHyun Min <contact@seunghyun.in>
2021-11-27 02:51:10 +0900

    feat: add views on home page

---
+++ lib/home/settings_view.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class SettingsView extends StatelessWidget {
+  const SettingsView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.center,
+      margin: const EdgeInsets.all(20),
+      child: const Text(
+        '설정 페이지가 업데이트될 예정입니다.',
+        textAlign: TextAlign.center,
+      ),
+    );
+  }
+}
