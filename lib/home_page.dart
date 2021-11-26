b17d81152acb04cc548dac4479d9481ddbbfa4da
SeungHyun Min <contact@seunghyun.in>
2021-11-27 02:36:54 +0900

    feat: change selected tab in home page

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -3,17 +3,36 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 final _backgroundColor = Colors.grey[50];
+const _tabItemNames = ['시험보기', '기록', '설정'];
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  Widget _buildBody() {
+    return Container();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: _backgroundColor,
       appBar: AppBar(
-        title: const Text(
-          '시험보기',
-          style: TextStyle(
+        title: Text(
+          _tabItemNames[_selectedIndex],
+          style: const TextStyle(
             fontFamily: 'NanumMyeongjo',
             fontSize: 28,
             fontWeight: FontWeight.w800,
@@ -27,23 +46,26 @@ class HomePage extends StatelessWidget {
         systemOverlayStyle: SystemUiOverlayStyle(
             statusBarColor: _backgroundColor, statusBarIconBrightness: Brightness.dark),
       ),
-      backgroundColor: _backgroundColor,
+      body: _buildBody(),
       bottomNavigationBar: BottomNavigationBar(
         elevation: 4,
         backgroundColor: Colors.white,
         showUnselectedLabels: false,
-        items: const [
+        showSelectedLabels: false,
+        onTap: _onItemTapped,
+        currentIndex: _selectedIndex,
+        items: [
           BottomNavigationBarItem(
-            icon: Icon(Icons.create),
-            label: '시험보기',
+            icon: const Icon(Icons.create),
+            label: _tabItemNames[0],
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.format_list_bulleted),
-            label: '기록',
+            icon: const Icon(Icons.format_list_bulleted),
+            label: _tabItemNames[1],
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.settings),
-            label: '설정',
+            icon: const Icon(Icons.settings),
+            label: _tabItemNames[2],
           ),
         ],
       ),
