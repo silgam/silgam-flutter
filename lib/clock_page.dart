66661f953c1f67d3d1b38ed41dc7a67c169c43d2
SeungHyun Min <contact@seunghyun.in>
2021-11-27 00:02:28 +0900

    feat(layout): basic layout of clock page

--- lib/clock_page.dart
+++ lib/clock_page.dart
@@ -1,12 +1,108 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 import 'analog_clock/analog_clock.dart';
 
-class ClockPage extends StatelessWidget {
+class ClockPage extends StatefulWidget {
   const ClockPage({Key? key}) : super(key: key);
 
   @override
+  State<ClockPage> createState() => _ClockPageState();
+}
+
+class _ClockPageState extends State<ClockPage> {
+  @override
+  void initState() {
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const AnalogClock();
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Column(
+        children: [
+          Expanded(
+            child: Container(
+              alignment: Alignment.topCenter,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    margin: const EdgeInsets.all(8),
+                    child: IconButton(
+                      splashRadius: 20,
+                      icon: const Icon(Icons.close),
+                      onPressed: _onCloseButtonPressed,
+                      color: Colors.white,
+                    ),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.only(right: 24),
+                    child: OutlinedButton(
+                      child: const Text(
+                        '건너뛰기',
+                        style: TextStyle(color: Colors.white, fontSize: 12),
+                      ),
+                      onPressed: _onSkipButtonPressed,
+                      style: OutlinedButton.styleFrom(
+                        side: const BorderSide(color: Colors.white),
+                        primary: Colors.white,
+                        tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                        minimumSize: Size.zero,
+                        padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 8),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          const WristWatch(),
+          Expanded(
+            child: Container(),
+          )
+        ],
+      ),
+    );
+  }
+
+  void _onCloseButtonPressed() {}
+
+  void _onSkipButtonPressed() {}
+
+  @override
+  void dispose() {
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
+    super.dispose();
+  }
+}
+
+class WristWatch extends StatelessWidget {
+  const WristWatch({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 200,
+      height: 330,
+      child: Stack(
+        children: [
+          SvgPicture.asset(
+            'assets/wrist_watch.svg',
+            fit: BoxFit.fitWidth,
+          ),
+          Container(
+            alignment: Alignment.center,
+            child: const FractionallySizedBox(
+              widthFactor: 0.77,
+              child: AnalogClock(borderWidth: 0),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
