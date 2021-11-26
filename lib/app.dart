012f617c98e83a7b2ce5b4bb0076ccdb5ffd85d0
SeungHyun Min <contact@seunghyun.in>
2021-11-27 01:04:55 +0900

    feat: set primary color swatch

--- lib/app.dart
+++ lib/app.dart
@@ -13,8 +13,28 @@ class SilgamApp extends StatelessWidget {
       routes: {
         '/clock': (context) => const ClockPage(),
       },
-      theme: ThemeData(fontFamily: 'NotoSansKR'),
+      theme: ThemeData(
+        primarySwatch: indigoSwatch,
+        fontFamily: 'NotoSansKR',
+      ),
       debugShowCheckedModeBanner: false,
     );
   }
 }
+
+const MaterialColor indigoSwatch = MaterialColor(
+  _indigoPrimaryValue,
+  <int, Color>{
+    50: Color(0xFFE8EAF6),
+    100: Color(0xFFC5CAE9),
+    200: Color(0xFF9FA8DA),
+    300: Color(0xFF7986CB),
+    400: Color(0xFF5C6BC0),
+    500: Color(0xFF3F51B5),
+    600: Color(0xFF3949AB),
+    700: Color(0xFF303F9F),
+    800: Color(_indigoPrimaryValue),
+    900: Color(0xFF1A237E),
+  },
+);
+const int _indigoPrimaryValue = 0xFF283593;
