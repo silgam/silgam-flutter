5946bea4b14f3d275f4b1412e9f63a3d678cc7e7
SeungHyun Min <contact@seunghyun.in>
2021-11-29 13:17:41 +0900

    refactor: create scaffold_body.dart

--- lib/home/settings_view.dart
+++ lib/home/settings_view.dart
@@ -1,17 +1,24 @@
 import 'package:flutter/material.dart';
 
+import '../util/scaffold_body.dart';
+
 class SettingsView extends StatelessWidget {
+  static const title = '설정';
+
   const SettingsView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return SliverFillRemaining(
-      child: Container(
-        alignment: Alignment.center,
-        margin: const EdgeInsets.all(20),
-        child: const Text(
-          '설정 페이지가 업데이트될 예정입니다.',
-          textAlign: TextAlign.center,
+    return ScaffoldBody(
+      title: title,
+      child: SliverFillRemaining(
+        child: Container(
+          alignment: Alignment.center,
+          margin: const EdgeInsets.all(20),
+          child: const Text(
+            '설정 페이지가 업데이트될 예정입니다.',
+            textAlign: TextAlign.center,
+          ),
         ),
       ),
     );
---
+++ lib/util/scaffold_body.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+import '../home/home_page.dart';
+
+class ScaffoldBody extends StatelessWidget {
+  final String title;
+  final Widget child;
+
+  const ScaffoldBody({
+    Key? key,
+    required this.title,
+    required this.child,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomScrollView(
+      physics: const BouncingScrollPhysics(),
+      slivers: [
+        SliverAppBar(
+          toolbarHeight: 80,
+          flexibleSpace: FlexibleSpaceBar(
+            titlePadding: const EdgeInsets.only(bottom: 16, left: 20),
+            centerTitle: false,
+            title: Text(
+              title,
+              style: const TextStyle(
+                fontFamily: 'NanumMyeongjo',
+                fontWeight: FontWeight.w800,
+                fontSize: 28,
+                color: Colors.black,
+              ),
+            ),
+          ),
+          foregroundColor: Colors.black,
+          backgroundColor: HomePage.backgroundColor,
+          systemOverlayStyle: const SystemUiOverlayStyle(
+            statusBarIconBrightness: Brightness.dark,
+            statusBarColor: Colors.transparent,
+          ),
+        ),
+        child,
+      ],
+    );
+  }
+}
