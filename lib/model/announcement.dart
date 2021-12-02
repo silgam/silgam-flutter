afb213390599ea52dd964b6271719f480bde282c
SeungHyun Min <contact@seunghyun.in>
2021-12-02 16:15:08 +0900

    feat: play announcement

--- lib/model/announcement.dart
+++ lib/model/announcement.dart
@@ -3,9 +3,11 @@ import 'relative_time.dart';
 class Announcement {
   final String title;
   final RelativeTime time;
+  final String? fileName;
 
   const Announcement({
     required this.time,
     required this.title,
+    this.fileName,
   });
 }
