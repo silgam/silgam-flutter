88692352676df47f7c7a406aa9e0968f78e499ab
SeungHyun Min <contact@seunghyun.in>
2021-12-04 23:18:49 +0900

    feat: integrate firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,10 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'app.dart';
 
-void main() => runApp(const SilgamApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const SilgamApp());
+}
