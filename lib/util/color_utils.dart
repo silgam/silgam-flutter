00363b3f67951335be6450799fbd070fb039431d
SeungHyun Min <contact@seunghyun.in>
2021-12-11 20:33:29 +0900

    feat: change gradient color of exams

--- lib/util/color_utils.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-Color getGradientColor(double t) {
-  final startColor = HSVColor.fromColor(const Color(0xFFC67EF2));
-  final endColor = HSVColor.fromColor(const Color(0xFF5CA2E8));
-  final color = HSVColor.lerp(startColor, endColor, t) ?? HSVColor.fromColor(Colors.white);
-  return color.toColor();
-}
