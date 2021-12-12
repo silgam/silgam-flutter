ce63d4143a6a436ffe5d9cf8b66699e727a8ef27
SeungHyun Min <contact@seunghyun.in>
2021-12-12 12:41:14 +0900

    feat: create login_page.dart

---
+++ lib/login_page/login_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  static const routeName = '/login';
+
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(),
+    );
+  }
+}
