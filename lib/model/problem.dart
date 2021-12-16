09aa6208986af6967ff5cb53258cb0595d25ef1d
SeungHyun Min <contact@seunghyun.in>
2021-12-16 15:53:26 +0900

    feat: make some models serializable

--- lib/model/problem.dart
+++ lib/model/problem.dart
@@ -1,19 +1,51 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'problem.g.dart';
+
+@JsonSerializable()
 class WrongProblem {
   final int problemNumber;
 
   WrongProblem(
     this.problemNumber,
   );
+
+  factory WrongProblem.fromJson(Map<String, dynamic> json) => _$WrongProblemFromJson(json);
+
+  Map<String, dynamic> toJson() => _$WrongProblemToJson(this);
+
+  static List<Map<String, dynamic>> toJsonList(List<WrongProblem> instances) {
+    return instances.map((e) => e.toJson()).toList();
+  }
+
+  @override
+  String toString() {
+    return 'WrongProblem{problemNumber: $problemNumber}';
+  }
 }
 
+@JsonSerializable()
 class ReviewProblem {
   final String title;
-  final String? memo;
+  final String memo;
   final List<String> imagePaths;
 
   ReviewProblem({
     required this.title,
-    this.memo,
+    this.memo = '',
     this.imagePaths = const [],
   });
+
+  factory ReviewProblem.fromJson(Map<String, dynamic> json) => _$ReviewProblemFromJson(json);
+
+  Map<String, dynamic> toJson() => _$ReviewProblemToJson(this);
+
+  static List<Map<String, dynamic>> toJsonList(List<ReviewProblem> instances) {
+    return instances.map((e) => e.toJson()).toList();
+  }
+
+  @override
+  String toString() {
+    return 'ReviewProblem{title: $title, memo: $memo, imagePaths: $imagePaths}';
+  }
 }
