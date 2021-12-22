a55e31889299c3c759b87f7ce5e59d3a972b30ac
SeungHyun Min <contact@seunghyun.in>
2021-12-22 11:24:05 +0900

    refactor: add first/second color properties on subject model

--- lib/model/subject.dart
+++ lib/model/subject.dart
@@ -24,4 +24,38 @@ extension SubjectExtension on Subject {
         return '제2외국어/한문';
     }
   }
+
+  int get firstColor {
+    switch (this) {
+      case Subject.language:
+        return 0xFF55B99E;
+      case Subject.math:
+        return 0xFFE05FA9;
+      case Subject.english:
+        return 0xFF0098C3;
+      case Subject.history:
+        return 0xFF7B4DB9;
+      case Subject.investigation:
+        return 0xFF7B4DB9;
+      case Subject.secondLanguage:
+        return 0xFFF39328;
+    }
+  }
+
+  int get secondColor {
+    switch (this) {
+      case Subject.language:
+        return 0xFF68D69B;
+      case Subject.math:
+        return 0xFFF574DD;
+      case Subject.english:
+        return 0xFF03BAEB;
+      case Subject.history:
+        return 0xFF8F6CE0;
+      case Subject.investigation:
+        return 0xFF8F6CE0;
+      case Subject.secondLanguage:
+        return 0xFFF7B061;
+    }
+  }
 }
