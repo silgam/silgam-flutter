d9291ae431ba8978340b4f210ccc02ae4da7f040
SeungHyun Min <contact@seunghyun.in>
2022-01-26 21:41:53 +0900

    feat: improve ux of edit record page

--- lib/edit_record_page/outlined_text_field.dart
+++ lib/edit_record_page/outlined_text_field.dart
@@ -1,19 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class OutlinedTextField extends StatelessWidget {
   final TextEditingController controller;
   final String suffix;
+  final int? maxLength;
 
   const OutlinedTextField({
     Key? key,
     required this.controller,
     required this.suffix,
+    this.maxLength,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return TextField(
       controller: controller,
+      inputFormatters: [LengthLimitingTextInputFormatter(maxLength)],
       keyboardType: TextInputType.number,
       textInputAction: TextInputAction.next,
       decoration: InputDecoration(
