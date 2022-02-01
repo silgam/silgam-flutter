5b66ecb908077ca5efb44cd3351ebab7173caadb
SeungHyun Min <contact@seunghyun.in>
2022-02-01 17:25:45 +0900

    feat: disable firebase analytics and crashlytics on debug mode

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 import 'app.dart';
@@ -7,6 +10,8 @@ import 'util/shared_preferences_holder.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  await FirebaseAnalytics.instance.setAnalyticsCollectionEnabled(kReleaseMode);
+  await FirebaseCrashlytics.instance.setCrashlyticsCollectionEnabled(kReleaseMode);
   await SharedPreferencesHolder.initializeSharedPreferences();
   runApp(const SilgamApp());
 }
