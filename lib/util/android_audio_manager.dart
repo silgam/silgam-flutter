32749746eea54615b3137075291199ee832e084f
SeungHyun Min <contact@seunghyun.in>
2022-02-06 14:42:02 +0900

    feat: run AndroidAudioManager on Android only

--- lib/util/android_audio_manager.dart
+++ lib/util/android_audio_manager.dart
@@ -1,13 +1,19 @@
+import 'dart:io';
+
 import 'package:flutter/services.dart';
 
 class AndroidAudioManager {
   static const _platform = MethodChannel('com.seunghyun.silgam/audio');
 
   static Future<void> controlMediaVolume() async {
-    await _platform.invokeMethod('controlMediaVolume');
+    if (Platform.isAndroid) {
+      await _platform.invokeMethod('controlMediaVolume');
+    }
   }
 
   static Future<void> controlDefaultVolume() async {
-    await _platform.invokeMethod('controlDefaultVolume');
+    if (Platform.isAndroid) {
+      await _platform.invokeMethod('controlDefaultVolume');
+    }
   }
 }
