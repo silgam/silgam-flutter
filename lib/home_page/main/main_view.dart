8d8a457cdc462ef70f96a38c75d92804c68258dc
SeungHyun Min <contact@seunghyun.in>
2022-02-12 00:07:54 +0900

    feat: update main view

--- lib/home_page/main/main_view.dart
+++ lib/home_page/main/main_view.dart
@@ -2,9 +2,7 @@ import 'package:flutter/material.dart';
 
 import '../../clock_page/clock_page.dart';
 import '../../model/exam.dart';
-import '../../model/subject.dart';
 import '../../repository/exam_repository.dart';
-import '../../util/scaffold_body.dart';
 
 class MainView extends StatelessWidget {
   static const title = '메인';
@@ -17,148 +15,226 @@ class MainView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ScaffoldBody(
-      title: title,
-      child: SliverPadding(
-        padding: const EdgeInsets.symmetric(vertical: 12),
-        sliver: SliverList(
-          delegate: SliverChildBuilderDelegate(
-            (context, index) => _ExamCard(
-              index: index,
-              numberOfItems: ExamRepository.defaultExams.length,
-              thisExam: ExamRepository.defaultExams[index],
-              navigateToRecordTab: navigateToRecordTab,
-            ),
-            childCount: ExamRepository.defaultExams.length,
-          ),
-        ),
-      ),
+    return Column(
+      children: [
+        _ExamStartCard(navigateToRecordTab: navigateToRecordTab),
+      ],
     );
   }
 }
 
-class _ExamCard extends StatefulWidget {
-  final int index;
-  final int numberOfItems;
-  final Exam thisExam;
+class _ExamStartCard extends StatefulWidget {
   final Function() navigateToRecordTab;
 
-  const _ExamCard({
-    required this.index,
-    required this.numberOfItems,
-    required this.thisExam,
-    required this.navigateToRecordTab,
+  const _ExamStartCard({
     Key? key,
+    required this.navigateToRecordTab,
   }) : super(key: key);
 
   @override
-  _ExamCardState createState() => _ExamCardState();
+  _ExamStartCardState createState() => _ExamStartCardState();
 }
 
-class _ExamCardState extends State<_ExamCard> {
+class _ExamStartCardState extends State<_ExamStartCard> with TickerProviderStateMixin {
+  late final TabController _subjectController;
+  Exam _selectedExam = ExamRepository.defaultExams[0];
+
+  @override
+  void initState() {
+    super.initState();
+    _subjectController = TabController(length: ExamRepository.defaultExams.length, vsync: this);
+    _subjectController.addListener(() {
+      final index = _subjectController.index;
+      _selectedExam = ExamRepository.defaultExams[index];
+      setState(() {});
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.symmetric(horizontal: 28, vertical: 10),
-      decoration: _buildGradientBoxDecoration(),
-      clipBehavior: Clip.hardEdge,
-      child: Material(
-        color: Colors.transparent,
-        child: InkWell(
-          splashColor: Colors.transparent,
-          highlightColor: Colors.white.withAlpha(30),
-          onTap: _onCardTapped,
-          child: Padding(
-            padding: const EdgeInsets.only(top: 12, bottom: 20, left: 20, right: 20),
-            child: Column(
+    Color disabledColor = Theme.of(context).primaryColor.withAlpha(80);
+    TextStyle? defaultTextStyle = Theme.of(context).primaryTextTheme.bodyLarge;
+    return _Card(
+      child: Column(
+        children: [
+          IntrinsicWidth(
+            child: Stack(
+              alignment: AlignmentDirectional.bottomCenter,
               children: [
-                Text(
-                  widget.thisExam.examName,
-                  style: const TextStyle(
-                    color: Colors.white,
-                    fontSize: 24,
-                    fontWeight: FontWeight.w400,
-                  ),
+                Container(
+                  height: 2,
+                  color: disabledColor,
                 ),
-                const SizedBox(height: 10),
-                _TextWithIcon(
-                  icon: Icons.schedule,
-                  text: widget.thisExam.buildExamTimeString(),
-                ),
-                const SizedBox(height: 4),
-                _TextWithIcon(
-                  icon: Icons.text_snippet,
-                  text: '${widget.thisExam.numberOfQuestions}문제 / ${widget.thisExam.perfectScore}점',
+                TabBar(
+                  controller: _subjectController,
+                  isScrollable: true,
+                  overlayColor: MaterialStateProperty.all(Colors.transparent),
+                  labelColor: Theme.of(context).primaryColor,
+                  unselectedLabelColor: disabledColor,
+                  labelStyle: defaultTextStyle?.copyWith(fontWeight: FontWeight.w900),
+                  unselectedLabelStyle: defaultTextStyle?.copyWith(fontWeight: FontWeight.w500),
+                  tabs: [
+                    for (Exam exam in ExamRepository.defaultExams) Tab(text: exam.examName),
+                  ],
                 ),
               ],
             ),
           ),
-        ),
+          const SizedBox(height: 32),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const SizedBox(width: 16),
+              Flexible(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    _buildInfo(
+                      iconData: Icons.schedule,
+                      title: '시험 시간',
+                      content: _selectedExam.getExamTimeString(),
+                    ),
+                    const SizedBox(height: 16),
+                    _buildInfo(
+                      iconData: Icons.text_snippet_outlined,
+                      title: '문제 수 / 만점',
+                      content: '${_selectedExam.numberOfQuestions}문제 / ${_selectedExam.perfectScore}점',
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(width: 20),
+              Ink(
+                width: 100,
+                height: 100,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  gradient: const LinearGradient(
+                    colors: [
+                      Color(0xFF1E2A7C),
+                      Color(0xFF283593),
+                    ],
+                  ),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withAlpha(16),
+                      offset: const Offset(0, 2),
+                      blurRadius: 10,
+                    ),
+                  ],
+                ),
+                child: InkWell(
+                  onTap: _onExamStartTap,
+                  splashColor: Colors.transparent,
+                  highlightColor: Colors.white.withAlpha(30),
+                  borderRadius: BorderRadius.circular(100),
+                  child: Container(
+                    alignment: Alignment.center,
+                    child: const Text(
+                      '시험시작',
+                      style: TextStyle(
+                        fontWeight: FontWeight.w900,
+                        color: Colors.white,
+                        fontSize: 18,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(width: 16),
+            ],
+          ),
+          const SizedBox(height: 32),
+        ],
       ),
     );
   }
 
-  void _onCardTapped() async {
+  Widget _buildInfo({
+    required IconData iconData,
+    required String title,
+    required String content,
+  }) {
+    Color color = const Color(0xFF081146);
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Icon(
+          iconData,
+          color: color,
+          size: 28,
+        ),
+        const SizedBox(width: 8),
+        Flexible(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                title,
+                style: TextStyle(
+                  color: color,
+                  fontWeight: FontWeight.w900,
+                  fontSize: 14,
+                ),
+              ),
+              const SizedBox(height: 4),
+              Text(
+                content,
+                style: TextStyle(
+                  color: color,
+                  fontSize: 12,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+
+  void _onExamStartTap() async {
     await Navigator.pushNamed(
       context,
       ClockPage.routeName,
-      arguments: ClockPageArguments(widget.thisExam),
+      arguments: ClockPageArguments(_selectedExam),
     );
     widget.navigateToRecordTab();
   }
 
-  BoxDecoration _buildGradientBoxDecoration() {
-    final Color startColor = Color(widget.thisExam.subject.firstColor);
-    final Color endColor = Color(widget.thisExam.subject.secondColor);
-    return BoxDecoration(
-      borderRadius: BorderRadius.circular(6),
-      gradient: LinearGradient(
-        begin: Alignment.centerLeft,
-        end: Alignment.centerRight,
-        colors: [
-          startColor,
-          endColor,
-        ],
-      ),
-      boxShadow: [
-        BoxShadow(
-          color: startColor.withAlpha(120),
-          blurRadius: 6,
-        ),
-      ],
-    );
+  @override
+  void dispose() {
+    super.dispose();
+    _subjectController.dispose();
   }
 }
 
-class _TextWithIcon extends StatelessWidget {
-  final IconData icon;
-  final String text;
+class _Card extends StatelessWidget {
+  final Widget child;
 
-  const _TextWithIcon({
+  const _Card({
     Key? key,
-    required this.icon,
-    required this.text,
+    required this.child,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Icon(
-          icon,
-          color: Colors.white,
-          size: 12,
-        ),
-        const SizedBox(width: 5),
-        Text(
-          text,
-          style: const TextStyle(
-            color: Colors.white,
-            fontSize: 12,
-            height: 1.2,
+    return Container(
+      clipBehavior: Clip.hardEdge,
+      margin: const EdgeInsets.all(20),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(12),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withAlpha(16),
+            offset: const Offset(0, 2),
+            blurRadius: 10,
           ),
-        ),
-      ],
+        ],
+      ),
+      child: Material(
+        child: child,
+      ),
     );
   }
 }
