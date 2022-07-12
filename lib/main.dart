1656a556cb637d9982579f921fdf78f8d47b6831
SeungHyun Min <contact@seunghyun.in>
2022-07-13 00:45:59 +0900

    chore: admob 초기 설정

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'package:kakao_flutter_sdk/kakao_flutter_sdk.dart';
 
 import 'app.dart';
@@ -15,5 +16,7 @@ void main() async {
   await FirebaseCrashlytics.instance.setCrashlyticsCollectionEnabled(kReleaseMode);
   await SharedPreferencesHolder.initializeSharedPreferences();
   KakaoSdk.init(nativeAppKey: "75edb119450e8355c4506a8623a2010e");
+  MobileAds.instance.initialize();
+
   runApp(const SilgamApp());
 }
