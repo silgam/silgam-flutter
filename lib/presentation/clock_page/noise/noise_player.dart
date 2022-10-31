ab509a605c7d1431065a085cd6a6f5a474978cab
Seunghyun Min <i@seunghyun.in>
2022-11-01 03:49:04 +0900

    fix: 시험 종료 직후 일시적으로 계속 소음 나오던 문제 수정

--- lib/presentation/clock_page/noise/noise_player.dart
+++ lib/presentation/clock_page/noise/noise_player.dart
@@ -15,37 +15,45 @@ abstract class NoisePlayer {
 }
 
 class NoiseAudioPlayer implements NoisePlayer {
-  AudioPlayer whiteNoisePlayer = AudioPlayer();
+  final AudioPlayer _whiteNoisePlayer = AudioPlayer();
+  final Map<int, AudioPlayer> _noisePlayers = {};
 
   @override
   void playNoise({required int noiseId, int delayMillis = 0}) async {
     Noise noise = Noise.byId(noiseId);
     String? noisePath = noise.getRandomNoisePath();
     if (noisePath == null) return;
+
     final audioPlayer = AudioPlayer();
+    int playerId = DateTime.now().millisecondsSinceEpoch;
+    _noisePlayers[playerId] = audioPlayer;
+
     await audioPlayer.setAsset(noisePath);
     double volume = (Random().nextDouble() + 2) * 2;
     await audioPlayer.setVolume(volume);
     await Future.delayed(Duration(milliseconds: delayMillis));
+
     await audioPlayer.play();
+    _noisePlayers.remove(playerId);
     await audioPlayer.dispose();
   }
 
   @override
   void playWhiteNoise() async {
-    await whiteNoisePlayer.setAsset(whiteNoisePath);
-    await whiteNoisePlayer.setLoopMode(LoopMode.all);
-    await whiteNoisePlayer.setVolume(2);
-    await whiteNoisePlayer.play();
+    await _whiteNoisePlayer.setAsset(whiteNoisePath);
+    await _whiteNoisePlayer.setLoopMode(LoopMode.all);
+    await _whiteNoisePlayer.setVolume(2);
+    await _whiteNoisePlayer.play();
   }
 
   @override
   void pauseWhiteNoise() async {
-    await whiteNoisePlayer.pause();
+    await _whiteNoisePlayer.pause();
   }
 
   @override
   void dispose() async {
-    await whiteNoisePlayer.dispose();
+    await _whiteNoisePlayer.dispose();
+    await Future.wait(_noisePlayers.values.map((player) => player.dispose()));
   }
 }
