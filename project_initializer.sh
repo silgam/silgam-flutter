293e3674471e45f535a311c967edcce18099cb82
Seunghyun Min <i@seunghyun.in>
2022-12-27 01:34:38 +0900

    chore: create project_initializer.sh

---
+++ project_initializer.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+set -e
+set -x
+
+curl -o ./silgam_private_files.zip $1
+unzip silgam_private_files.zip
+
+mv silgam_private_files/announcements ./assets/announcements
+mv silgam_private_files/noises ./assets/noises
+mv silgam_private_files/app_env.dart ./lib/app_env.dart
+mv silgam_private_files/firebase_options.dart ./lib/firebase_options.dart
+mv silgam_private_files/GoogleService-Info.plist ./ios/Runner/GoogleService-Info.plist
+mv silgam_private_files/google-services.json ./android/app/google-services.json
+
+flutter pub run build_runner build --delete-conflicting-outputs
