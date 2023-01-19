bc1312787638c7a0f1b4e7ff5c3fa71e636b03e8
Seunghyun Min <i@seunghyun.in>
2023-01-20 04:36:19 +0900

    feat: 복습할 문제 카드 디자인 개선

--- lib/presentation/common/review_problem_card.dart
+++ lib/presentation/common/review_problem_card.dart
@@ -33,59 +33,60 @@ class ReviewProblemCard extends StatelessWidget {
           side: BorderSide(width: 0.5, color: Colors.grey.shade300),
           borderRadius: const BorderRadius.all(Radius.circular(8)),
         ),
-        child: Stack(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             if (problem.imagePaths.isNotEmpty)
-              SizedBox(
-                width: double.infinity,
-                height: double.infinity,
-                child: Builder(builder: (context) {
-                  String imagePath = problem.imagePaths.first;
-                  if (imagePath.startsWith('http')) {
-                    return Image.network(
-                      imagePath,
-                      width: double.infinity,
-                      fit: BoxFit.cover,
-                      alignment: Alignment.topCenter,
-                      loadingBuilder: (_, child, loadingProgress) {
-                        if (loadingProgress == null) return child;
-                        return const Center(
-                          child: CircularProgressIndicator(strokeWidth: 2),
-                        );
-                      },
-                    );
-                  } else {
-                    return Image.file(
-                      File(imagePath),
-                      width: double.infinity,
-                      fit: BoxFit.cover,
-                      alignment: Alignment.topCenter,
-                    );
-                  }
-                }),
-              ),
-            if (problem.imagePaths.isEmpty)
-              Container(
-                alignment: Alignment.center,
-                child: Image.asset(
-                  'assets/app_icon/app_icon_transparent.png',
-                  width: 100,
-                  color: Colors.grey.shade100,
+              Expanded(
+                child: SizedBox(
+                  width: double.infinity,
+                  height: double.infinity,
+                  child: Builder(builder: (context) {
+                    String imagePath = problem.imagePaths.first;
+                    if (imagePath.startsWith('http')) {
+                      return Image.network(
+                        imagePath,
+                        width: double.infinity,
+                        fit: BoxFit.cover,
+                        alignment: Alignment.topCenter,
+                        loadingBuilder: (_, child, loadingProgress) {
+                          if (loadingProgress == null) return child;
+                          return const Center(
+                            child: CircularProgressIndicator(strokeWidth: 2),
+                          );
+                        },
+                      );
+                    } else {
+                      return Image.file(
+                        File(imagePath),
+                        width: double.infinity,
+                        fit: BoxFit.cover,
+                        alignment: Alignment.topCenter,
+                      );
+                    }
+                  }),
                 ),
               ),
-            Container(
-              margin: const EdgeInsets.all(8),
-              padding: const EdgeInsets.symmetric(vertical: 6, horizontal: 10),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(8),
-                color: Colors.white.withAlpha(200),
+            if (problem.imagePaths.isEmpty)
+              Expanded(
+                child: Container(
+                  alignment: Alignment.center,
+                  child: Image.asset(
+                    'assets/app_icon/app_icon_transparent.png',
+                    width: 100,
+                    color: Colors.grey.shade100,
+                  ),
+                ),
               ),
+            const SizedBox(height: 2),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
               child: Row(
                 mainAxisSize: MainAxisSize.min,
                 crossAxisAlignment: CrossAxisAlignment.baseline,
                 textBaseline: TextBaseline.ideographic,
                 children: [
-                  Flexible(
+                  Expanded(
                     child: Text(
                       problem.title,
                       maxLines: 1,
@@ -109,6 +110,21 @@ class ReviewProblemCard extends StatelessWidget {
                 ],
               ),
             ),
+            if (problem.memo.isNotEmpty)
+              Padding(
+                padding: const EdgeInsets.only(left: 8, right: 8, bottom: 4),
+                child: Text(
+                  problem.memo.replaceAll('\n', ' '),
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                  style: const TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.w300,
+                    fontSize: 13,
+                  ),
+                ),
+              ),
+            const SizedBox(height: 4),
           ],
         ),
       ),
