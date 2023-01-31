58997248442393e5974d0f772ad07d8d42b7a0f8
Seunghyun Min <i@seunghyun.in>
2023-01-31 20:13:21 +0900

    feat: 개수 제한 dialog 내용 수정

--- lib/presentation/common/dialog.dart
+++ lib/presentation/common/dialog.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:silgam/presentation/app/cubit/iap_cubit.dart';
 
+import '../app/cubit/app_cubit.dart';
+import '../app/cubit/iap_cubit.dart';
 import '../purchase_page/purchase_page.dart';
 
 void showExamRecordLimitInfoDialog(BuildContext context) {
@@ -11,37 +12,42 @@ void showExamRecordLimitInfoDialog(BuildContext context) {
       name: '/record_list/limit_help_dialog',
     ),
     builder: (context) {
-      return BlocBuilder<IapCubit, IapState>(
-        builder: (context, iapState) {
-          return AlertDialog(
-            title: const Text(
-              '개수 제한 안내 제목',
-              style: TextStyle(fontWeight: FontWeight.w700),
-            ),
-            content: const Text('설명'),
-            actions: [
-              TextButton(
-                onPressed: () {
-                  Navigator.of(context).pop();
-                },
-                style: TextButton.styleFrom(
-                  foregroundColor: Colors.grey,
+      return BlocBuilder<AppCubit, AppState>(
+        builder: (context, appState) {
+          return BlocBuilder<IapCubit, IapState>(
+            builder: (context, iapState) {
+              return AlertDialog(
+                title: const Text(
+                  '실모 기록 개수 제한 안내',
+                  style: TextStyle(fontWeight: FontWeight.w700),
                 ),
-                child: const Text('확인'),
-              ),
-              TextButton(
-                onPressed: () {
-                  Navigator.of(context).pop();
-                  Navigator.of(context).pushNamed(
-                    PurchasePage.routeName,
-                    arguments: PurchasePageArguments(
-                      product: iapState.activeProducts.first,
+                content: Text(
+                    '실감패스를 이용하시기 전까지는 실모 기록을 ${appState.productBenefit.examRecordLimit}개까지만 저장하실 수 있어요 😭'),
+                actions: [
+                  TextButton(
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                    style: TextButton.styleFrom(
+                      foregroundColor: Colors.grey,
                     ),
-                  );
-                },
-                child: const Text('실감패스 확인하러 가기'),
-              ),
-            ],
+                    child: const Text('확인'),
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                      Navigator.of(context).pushNamed(
+                        PurchasePage.routeName,
+                        arguments: PurchasePageArguments(
+                          product: iapState.activeProducts.first,
+                        ),
+                      );
+                    },
+                    child: const Text('실감패스 확인하러 가기'),
+                  ),
+                ],
+              );
+            },
           );
         },
       );
