76b9a5b7f5b3bccedafbe02250873024aafbb67f
Seunghyun Min <i@seunghyun.in>
2023-10-11 00:15:24 +0900

    fix: prevent emitting after calling close in PurchaseCubit

--- lib/presentation/purchase_page/cubit/purchase_cubit.dart
+++ lib/presentation/purchase_page/cubit/purchase_cubit.dart
@@ -10,14 +10,17 @@ class PurchaseCubit extends Cubit<PurchaseState> {
   PurchaseCubit() : super(const PurchaseState());
 
   void onWebviewProgressChanged(int progress) {
+    if (isClosed) return;
     emit(state.copyWith(isWebviewLoading: progress < 100));
   }
 
   void purchaseSectionShown() {
+    if (isClosed) return;
     emit(state.copyWith(isPurchaseSectionShown: true));
   }
 
   void purchaseSectionHidden() {
+    if (isClosed) return;
     emit(state.copyWith(isPurchaseSectionShown: false));
   }
 }
