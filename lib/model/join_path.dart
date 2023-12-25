e016bba21efc7a27204131104bc8af92acfb6172
Seunghyun Min <i@seunghyun.in>
2023-12-25 19:12:48 +0900

    Implement OnboardingRepository

---
+++ lib/model/join_path.dart
@@ -0,0 +1,15 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'join_path.freezed.dart';
+part 'join_path.g.dart';
+
+@freezed
+class JoinPath with _$JoinPath {
+  const factory JoinPath({
+    required String id,
+    required String text,
+  }) = _JoinPath;
+
+  factory JoinPath.fromJson(Map<String, dynamic> json) =>
+      _$JoinPathFromJson(json);
+}
