00a41e4ba7f11536d0697f9705d9e1b40c4ad402
Seunghyun Min <i@seunghyun.in>
2024-05-12 01:26:38 +0900

    Navigate to custom exam edit page on click item

--- lib/presentation/custom_exam_list/custom_exam_list_page.dart
+++ lib/presentation/custom_exam_list/custom_exam_list_page.dart
@@ -3,11 +3,21 @@ import 'package:flutter/material.dart';
 import '../../model/exam.dart';
 import '../../repository/exam/exam_repository.dart';
 import '../common/custom_menu_bar.dart';
+import '../custom_exam_edit/custom_exam_edit_page.dart';
 
-class CustomExamListPage extends StatelessWidget {
+class CustomExamListPage extends StatefulWidget {
   static const routeName = '/custom_exam';
 
   const CustomExamListPage({super.key});
+
+  @override
+  State<CustomExamListPage> createState() => _CustomExamListPageState();
+}
+
+class _CustomExamListPageState extends State<CustomExamListPage> {
+  void _onTabCustomExamItem(Exam exam) {
+    Navigator.pushNamed(context, CustomExamEditPage.routeName);
+  }
 
   Widget _buildExamInfoWidget(String text) {
     return Text(
@@ -20,44 +30,52 @@ class CustomExamListPage extends StatelessWidget {
   }
 
   Widget _buildCustomExamItem(Exam exam) {
-    return Container(
-      margin: const EdgeInsets.symmetric(vertical: 4, horizontal: 20),
-      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
-      color: Colors.white,
-      child: Row(
-        children: [
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 20),
+      child: InkWell(
+        onTap: () => _onTabCustomExamItem(exam),
+        splashColor: Colors.transparent,
+        child: Ink(
+          color: Colors.white,
+          child: Container(
+            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+            child: Row(
               children: [
-                Text(
-                  exam.subject.defaultExam.name,
-                  style: TextStyle(
-                    color: Color(exam.color),
-                    fontSize: 12,
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        exam.subject.defaultExam.name,
+                        style: TextStyle(
+                          color: Color(exam.color),
+                          fontSize: 12,
+                        ),
+                      ),
+                      const SizedBox(height: 2),
+                      Text(
+                        exam.name,
+                        style: const TextStyle(
+                          fontWeight: FontWeight.w600,
+                          fontSize: 18,
+                        ),
+                      ),
+                    ],
                   ),
                 ),
-                const SizedBox(height: 2),
-                Text(
-                  exam.name,
-                  style: const TextStyle(
-                    fontWeight: FontWeight.w600,
-                    fontSize: 18,
-                  ),
+                const SizedBox(width: 12),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    _buildExamInfoWidget(
+                        '${exam.startTime.hour}:${exam.startTime.minute} ~'),
+                    _buildExamInfoWidget('${exam.durationMinutes}분'),
+                  ],
                 ),
               ],
             ),
           ),
-          const SizedBox(width: 12),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.end,
-            children: [
-              _buildExamInfoWidget(
-                  '${exam.startTime.hour}:${exam.startTime.minute} ~'),
-              _buildExamInfoWidget('${exam.durationMinutes}분'),
-            ],
-          ),
-        ],
+        ),
       ),
     );
   }
