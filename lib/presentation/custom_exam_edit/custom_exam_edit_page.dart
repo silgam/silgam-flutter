d2f6d11926c57b3bde4760109923da05ead22361
Seunghyun Min <i@seunghyun.in>
2024-05-12 00:13:30 +0900

    Create route name of custom exam edit page using list page

--- lib/presentation/custom_exam_edit/custom_exam_edit_page.dart
+++ lib/presentation/custom_exam_edit/custom_exam_edit_page.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:silgam/presentation/custom_exam_list/custom_exam_list_page.dart';
 
 class CustomExamEditPage extends StatelessWidget {
-  static const routeName = '/custom_exam/edit';
+  static const routeName = '${CustomExamListPage.routeName}/edit';
 
   const CustomExamEditPage({super.key});
 
