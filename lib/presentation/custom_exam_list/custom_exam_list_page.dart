f2de5568eaa23e80f5c831ff020c1e95e0299910
Seunghyun Min <i@seunghyun.in>
2024-05-12 02:18:49 +0900

    Show exam info in exam item

--- lib/presentation/custom_exam_list/custom_exam_list_page.dart
+++ lib/presentation/custom_exam_list/custom_exam_list_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 import '../../model/exam.dart';
 import '../../repository/exam/exam_repository.dart';
@@ -19,13 +20,24 @@ class _CustomExamListPageState extends State<CustomExamListPage> {
     Navigator.pushNamed(context, CustomExamEditPage.routeName);
   }
 
-  Widget _buildExamInfoWidget(String text) {
-    return Text(
-      text,
-      style: TextStyle(
-        color: Colors.grey.shade700,
-        fontSize: 12,
-      ),
+  Widget _buildExamInfoWidget(IconData iconData, String text) {
+    return Row(
+      children: [
+        Icon(
+          iconData,
+          size: 16,
+          color: Colors.grey.shade800,
+        ),
+        const SizedBox(width: 4),
+        Text(
+          text,
+          style: TextStyle(
+            color: Colors.grey.shade800,
+            fontSize: 12,
+            height: 1.2,
+          ),
+        ),
+      ],
     );
   }
 
@@ -55,7 +67,9 @@ class _CustomExamListPageState extends State<CustomExamListPage> {
                       const SizedBox(height: 2),
                       Text(
                         exam.name,
+                        maxLines: 2,
                         style: const TextStyle(
+                          overflow: TextOverflow.ellipsis,
                           fontWeight: FontWeight.w600,
                           fontSize: 18,
                         ),
@@ -68,8 +82,14 @@ class _CustomExamListPageState extends State<CustomExamListPage> {
                   crossAxisAlignment: CrossAxisAlignment.end,
                   children: [
                     _buildExamInfoWidget(
-                        '${exam.startTime.hour}:${exam.startTime.minute} ~'),
-                    _buildExamInfoWidget('${exam.durationMinutes}분'),
+                      Icons.schedule,
+                      '${DateFormat.Hm().format(exam.startTime)} ~ ${DateFormat.Hm().format(exam.endTime)} (${exam.durationMinutes}분)',
+                    ),
+                    const SizedBox(height: 2),
+                    _buildExamInfoWidget(
+                      Icons.text_snippet_outlined,
+                      '${exam.numberOfQuestions}문제 / ${exam.perfectScore}점',
+                    ),
                   ],
                 ),
               ],
