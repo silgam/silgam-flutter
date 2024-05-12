b8ff4e684edab36cdd1f15d0a8b0a5bf86a800c4
Seunghyun Min <i@seunghyun.in>
2024-05-12 22:28:32 +0900

    Remove margin from snack bar

--- lib/presentation/custom_exam_edit/custom_exam_edit_page.dart
+++ lib/presentation/custom_exam_edit/custom_exam_edit_page.dart
@@ -105,7 +105,6 @@ class _CustomExamEditPageState extends State<CustomExamEditPage> {
         ScaffoldMessenger.of(context).showSnackBar(
           SnackBar(
             behavior: SnackBarBehavior.floating,
-            margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 60),
             duration: const Duration(seconds: 2),
             content: Text(firstErrorMessage),
           ),
