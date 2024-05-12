7f9e2f2a030726886265a8956634e820867c1adf
Seunghyun Min <i@seunghyun.in>
2024-05-12 18:25:01 +0900

    Add some form fields

--- lib/presentation/custom_exam_edit/custom_exam_edit_page.dart
+++ lib/presentation/custom_exam_edit/custom_exam_edit_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:silgam/presentation/custom_exam_list/custom_exam_list_page.dart';
 
 class CustomExamEditPage extends StatefulWidget {
@@ -13,6 +14,30 @@ class CustomExamEditPage extends StatefulWidget {
 class _CustomExamEditPageState extends State<CustomExamEditPage> {
   final _formKey = GlobalKey<FormState>();
   bool _isChanged = false;
+
+  late final defaultInputDecoration = InputDecoration(
+    hintStyle: TextStyle(color: Colors.grey.shade500),
+    filled: true,
+    fillColor: Colors.white,
+    contentPadding: const EdgeInsets.all(12),
+    isCollapsed: true,
+    enabledBorder: OutlineInputBorder(
+      borderSide: BorderSide(width: 0.5, color: Colors.grey.shade300),
+      borderRadius: const BorderRadius.all(Radius.circular(6)),
+    ),
+    focusedBorder: OutlineInputBorder(
+      borderSide: BorderSide(width: 0.5, color: Theme.of(context).primaryColor),
+      borderRadius: const BorderRadius.all(Radius.circular(6)),
+    ),
+    errorBorder: const OutlineInputBorder(
+      borderSide: BorderSide(width: 0.5, color: Colors.red),
+      borderRadius: BorderRadius.all(Radius.circular(6)),
+    ),
+    focusedErrorBorder: const OutlineInputBorder(
+      borderSide: BorderSide(width: 0.5, color: Colors.red),
+      borderRadius: BorderRadius.all(Radius.circular(6)),
+    ),
+  );
 
   Future<bool> _onWillPop() async {
     if (!_isChanged) {
@@ -69,6 +94,50 @@ class _CustomExamEditPageState extends State<CustomExamEditPage> {
     }
   }
 
+  Widget _buildLabel(String text) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 6),
+      child: Text(
+        text,
+        style: TextStyle(
+          color: Colors.grey.shade500,
+          fontWeight: FontWeight.w500,
+        ),
+      ),
+    );
+  }
+
+  Widget _buildNumberInput({
+    required String label,
+    required String suffix,
+    required int maxLength,
+    required double inputWidth,
+    required FormFieldValidator validator,
+  }) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        _buildLabel(label),
+        SizedBox(
+          width: inputWidth,
+          child: TextFormField(
+            autovalidateMode: AutovalidateMode.onUserInteraction,
+            textInputAction: TextInputAction.next,
+            keyboardType: TextInputType.number,
+            validator: validator,
+            inputFormatters: [
+              FilteringTextInputFormatter.digitsOnly,
+              LengthLimitingTextInputFormatter(maxLength),
+            ],
+            decoration: defaultInputDecoration.copyWith(
+              suffixText: suffix,
+            ),
+          ),
+        )
+      ],
+    );
+  }
+
   Widget _buildForm() {
     return Form(
       key: _formKey,
@@ -80,23 +149,81 @@ class _CustomExamEditPageState extends State<CustomExamEditPage> {
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           const SizedBox(height: 28),
+          _buildLabel('과목 이름'),
           TextFormField(
+            autovalidateMode: AutovalidateMode.onUserInteraction,
+            textInputAction: TextInputAction.next,
             validator: (value) {
               if (value == null || value.isEmpty) {
                 return '과목 이름을 입력해주세요.';
               }
               return null;
             },
-            decoration: const InputDecoration(
-              hintText: 'test',
-              label: Text('과목 이름'),
+            decoration: defaultInputDecoration.copyWith(
+              hintText: '실감 하프 모의고사',
+            ),
+            style: const TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.w700,
             ),
           ),
-          TextFormField(
-            decoration: const InputDecoration(
-              hintText: 'test',
-              label: Text('과목 이름'),
-            ),
+          const SizedBox(height: 20),
+          Wrap(
+            spacing: 12,
+            runSpacing: 12,
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  _buildLabel('시험 시작 시간'),
+                  GestureDetector(
+                    onTap: () {},
+                    child: Container(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 12,
+                        vertical: 12,
+                      ),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(6),
+                        border: Border.all(
+                          color: Colors.grey.shade300,
+                          width: 0.5,
+                        ),
+                      ),
+                      child: const Text(
+                        // DateFormat.jm('ko_KR').format(_examStartedTime),
+                        '오전 10:00',
+                        style: TextStyle(
+                          fontSize: 17,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              _buildNumberInput(
+                label: '시험 시간',
+                suffix: '분',
+                maxLength: 3,
+                inputWidth: 75,
+                validator: (value) => null,
+              ),
+              _buildNumberInput(
+                label: '문제 수',
+                suffix: '문제',
+                maxLength: 3,
+                inputWidth: 90,
+                validator: (value) => null,
+              ),
+              _buildNumberInput(
+                label: '만점',
+                suffix: '점',
+                maxLength: 3,
+                inputWidth: 75,
+                validator: (value) => null,
+              ),
+            ],
           ),
         ],
       ),
