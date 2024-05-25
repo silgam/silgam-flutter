86581776d035ce0d31572ce58e8fb97795af09c2
Seunghyun Min <i@seunghyun.in>
2024-05-25 17:55:33 +0900

    Create ExamRepository

--- lib/repository/exam/exam_repository.dart
+++ lib/repository/exam/exam_repository.dart
@@ -1,6 +1,36 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:injectable/injectable.dart';
+
 import '../../model/exam.dart';
 import '../../model/subject.dart';
 import '../../util/date_time_extension.dart';
+
+@lazySingleton
+class ExamRepository {
+  final CollectionReference<Exam> _examsCollection =
+      FirebaseFirestore.instance.collection('exams').withConverter(
+            fromFirestore: (snapshot, _) => Exam.fromJson(snapshot.data()!),
+            toFirestore: (exam, _) => exam.toJson(),
+          );
+
+  void addExam(Exam exam) {
+    _examsCollection.doc(exam.id).set(exam);
+  }
+
+  Future<List<Exam>> getMyExams(String userId) async {
+    final exams =
+        await _examsCollection.where('userId', isEqualTo: userId).get();
+    return exams.docs.map((snapshot) => snapshot.data()).toList();
+  }
+
+  void updateExam(Exam exam) {
+    _examsCollection.doc(exam.id).update(exam.toJson());
+  }
+
+  void deleteExam(String examId) {
+    _examsCollection.doc(examId).delete();
+  }
+}
 
 final List<Exam> defaultExams = [
   Exam(
