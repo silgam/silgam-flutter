113054959589a60772426115b89318a50ea12f20
Seunghyun Min <i@seunghyun.in>
2025-03-17 17:32:15 +0900

    Refactor NoiseGenerator to extract timer logic into a separate method

--- lib/presentation/clock/noise/noise_generator.dart
+++ lib/presentation/clock/noise/noise_generator.dart
@@ -26,48 +26,54 @@ class NoiseGenerator {
   void start() {
     playWhiteNoiseIfEnabled();
 
-    _timer = Timer.periodic(const Duration(milliseconds: 100), (_) async {
-      ClockState clockState = clockCubit.state;
-      if (!clockState.isRunning) return;
-      RelativeTimeType currentRelativeTime = clockState.currentBreakpoint.announcement.time.type;
-      for (final MapEntry(key: id, value: level) in noiseSettingState.noiseLevels.entries) {
-        double levelMultiple = 1;
-        int delay = 0;
-        // 시험지 넘기는 소리 예외 사항
-        if (id == 0) {
-          if (currentRelativeTime == RelativeTimeType.beforeStart) {
-            levelMultiple = 0; // 시험 시작 전엔 시험지 안 넘김
-          } else if (currentRelativeTime == RelativeTimeType.afterStart) {
-            int afterStart =
-                clockState.currentTime.difference(clockState.currentBreakpoint.time).inSeconds;
-            if (afterStart <= 2) {
-              delay = 1000;
-              levelMultiple = 50; // 시험 시작 직후 시험지 많이 넘김
-            } else if (2 < afterStart && afterStart <= 7) {
-              delay = 1000;
-              levelMultiple = 10; // 시험 시작 후 일정 시간 동안 시험지 조금 넘김
-            }
-          } else if (currentRelativeTime == RelativeTimeType.beforeFinish) {
-            int beforeFinish =
-                clockState.currentTime.difference(clockState.currentBreakpoint.time).inMinutes;
-            if (clockState.currentExam.subject == Subject.investigation ||
-                clockState.currentExam.subject == Subject.investigation2) {
-              beforeFinish = 5 - beforeFinish;
-            } else {
-              beforeFinish = 10 - beforeFinish;
-            }
-            if (beforeFinish <= 2) {
-              levelMultiple = 10; // 시험 종료 직전 시험지 많이 넘김
-            } else if (2 < beforeFinish && beforeFinish <= 10) {
-              levelMultiple = 2; // 시험 종료 전 일정 시간 동안 시험지 조금 넘김
-            }
+    _timer = Timer.periodic(const Duration(milliseconds: 100), _onEveryTick);
+  }
+
+  void _onEveryTick(Timer timer) async {
+    ClockState clockState = clockCubit.state;
+    if (!clockState.isRunning) return;
+
+    RelativeTimeType currentRelativeTime = clockState.currentBreakpoint.announcement.time.type;
+
+    for (final MapEntry(key: id, value: level) in noiseSettingState.noiseLevels.entries) {
+      double levelMultiple = 1;
+      int delay = 0;
+
+      // 시험지 넘기는 소리 예외 사항
+      if (id == 0) {
+        if (currentRelativeTime == RelativeTimeType.beforeStart) {
+          levelMultiple = 0; // 시험 시작 전엔 시험지 안 넘김
+        } else if (currentRelativeTime == RelativeTimeType.afterStart) {
+          int afterStart =
+              clockState.currentTime.difference(clockState.currentBreakpoint.time).inSeconds;
+          if (afterStart <= 2) {
+            delay = 1000;
+            levelMultiple = 50; // 시험 시작 직후 시험지 많이 넘김
+          } else if (2 < afterStart && afterStart <= 7) {
+            delay = 1000;
+            levelMultiple = 10; // 시험 시작 후 일정 시간 동안 시험지 조금 넘김
+          }
+        } else if (currentRelativeTime == RelativeTimeType.beforeFinish) {
+          int beforeFinish =
+              clockState.currentTime.difference(clockState.currentBreakpoint.time).inMinutes;
+          if (clockState.currentExam.subject == Subject.investigation ||
+              clockState.currentExam.subject == Subject.investigation2) {
+            beforeFinish = 5 - beforeFinish;
+          } else {
+            beforeFinish = 10 - beforeFinish;
+          }
+          if (beforeFinish <= 2) {
+            levelMultiple = 10; // 시험 종료 직전 시험지 많이 넘김
+          } else if (2 < beforeFinish && beforeFinish <= 10) {
+            levelMultiple = 2; // 시험 종료 전 일정 시간 동안 시험지 조금 넘김
           }
         }
-        if (_calculateProbability(level * levelMultiple)) {
-          await noisePlayer.playNoise(noiseId: id, delayMillis: delay);
-        }
       }
-    });
+
+      if (_calculateProbability(level * levelMultiple)) {
+        await noisePlayer.playNoise(noiseId: id, delayMillis: delay);
+      }
+    }
   }
 
   void playWhiteNoiseIfEnabled() {
