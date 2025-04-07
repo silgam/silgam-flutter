63bbeb1338e0f1361e40b2e256ae7cfdd47b483f
Seunghyun Min <i@seunghyun.in>
2025-04-07 18:04:40 +0900

    오프라인 상태에서 띄워야 하는 배너가 바뀌지 않던 문제 수정

--- lib/repository/ads/ads_repository.dart
+++ lib/repository/ads/ads_repository.dart
@@ -1,10 +1,8 @@
 import 'dart:developer';
 
-import 'package:collection/collection.dart';
 import 'package:dio/dio.dart';
 import 'package:injectable/injectable.dart';
 import 'package:multiple_result/multiple_result.dart';
-import 'package:package_info_plus/package_info_plus.dart';
 
 import '../../model/ads.dart';
 import '../../util/api_failure.dart';
@@ -19,45 +17,10 @@ class AdsRepository {
   Future<Result<List<Ads>, ApiFailure>> getAllAds() async {
     try {
       var ads = await _adsApi.getAllAds();
-      final filteredAds = await _getFilteredAndSortedAds(ads);
-
-      return Result.success(filteredAds);
+      return Result.success(ads);
     } on DioException catch (e) {
       log(e.toString(), name: 'AdsRepository.getAllAds');
       return Result.error(e.error as ApiFailure);
     }
-  }
-
-  Future<List<Ads>> _getFilteredAndSortedAds(List<Ads> ads) async {
-    final versionNumber = await _getVersionNumber();
-
-    return ads
-        .where(
-          (ad) =>
-              ad.minVersionNumber <= versionNumber &&
-              (ad.maxVersionNumber == null || ad.maxVersionNumber! >= versionNumber) &&
-              ad.expiryDate.isAfter(DateTime.now()) &&
-              ad.startDate.isBefore(DateTime.now()),
-        )
-        .groupListsBy((ad) => ad.category)
-        .entries
-        .expand((entry) {
-          final category = entry.key;
-          final categoryAds = entry.value;
-          final showCount = categoryAds.firstOrNull?.showCountInCategory;
-
-          if (category == null || showCount == null) {
-            return categoryAds;
-          }
-
-          return categoryAds.shuffled().take(showCount);
-        })
-        .shuffled()
-        .sorted((a, b) => a.priority - b.priority);
-  }
-
-  Future<int> _getVersionNumber() async {
-    final packageInfo = await PackageInfo.fromPlatform();
-    return int.parse(packageInfo.buildNumber);
   }
 }
